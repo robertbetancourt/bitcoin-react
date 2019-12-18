ab3314b3a0cf1e689bed257f5b9d8a7564c8d9c9
Robert <robertbetancourt212@gmail.com>
2019-12-18 19:23:52 -0400

    Updates

---
+++ precache-manifest.5c23c10be717c809ab66403765000408.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84907ea658f3f6600ab493d0b0787be",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "e4bc284eee73e99d8394",
+    "url": "/bitcoin-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ab1f5e461a4bd61d0b93",
+    "url": "/bitcoin-react/static/css/main.b523a2d2.chunk.css"
+  },
+  {
+    "revision": "e4bc284eee73e99d8394",
+    "url": "/bitcoin-react/static/js/2.b198bae7.chunk.js"
+  },
+  {
+    "revision": "c11464f8ef9f75920a8a4b3f7babed1a",
+    "url": "/bitcoin-react/static/js/2.b198bae7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab1f5e461a4bd61d0b93",
+    "url": "/bitcoin-react/static/js/main.c7d2ddd0.chunk.js"
+  },
+  {
+    "revision": "272951f3765563e0b30f",
+    "url": "/bitcoin-react/static/js/runtime-main.6786a0d6.js"
+  },
+  {
+    "revision": "91686f14b1af26c19a022f7630e01c0d",
+    "url": "/bitcoin-react/static/media/br.91686f14.png"
+  },
+  {
+    "revision": "cfdbafe37f7253f018b83eb689f02d2e",
+    "url": "/bitcoin-react/static/media/cancelar.cfdbafe3.png"
+  }
+]);
